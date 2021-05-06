dd02ef72fc217ccffb7a166a2b1af8e5d2e9f9a3
Pritam Majumder <ptmmajumder630@gmail.com>
2021-05-07 03:18:15 +0530

    update

--- src/script.js
+++ src/script.js
@@ -30,22 +30,17 @@ const navBtn1 = document.querySelector(".nav-btn1");
 const navBtn2 = document.querySelector(".nav-btn2");
 const navBtn3 = document.querySelector(".nav-btn3");
 const navBtn4 = document.querySelector(".nav-btn4");
-const navBtn5 = document.querySelector(".nav-btn5");
 
 const about = document.querySelector(".row");
 const skills = document.querySelector(".skills");
 const portfolio = document.querySelector(".portfolios");
-const services = document.querySelector(".services");
 const contact = document.querySelector(".contact");
 
 navBtn2.addEventListener('click',() =>{
 portfolio.classList.remove("sec-dis")
 about.classList.remove("sec-dis")
-services.classList.remove("sec-dis")
 
 contact.classList.remove("sec-dis")
-
-navBtn5.classList.remove("active")
 
 navBtn1.classList.remove("active")
 navBtn3.classList.remove("active")
@@ -65,8 +60,6 @@ navBtn3.classList.remove("active")
 navBtn4.classList.remove("active")
 contact.classList.remove("sec-dis")
 
-navBtn5.classList.remove("active")
-services.classList.remove("sec-dis")
 navBtn2.classList.remove("active")
 skills.classList.remove("sec-dis")
 nav.classList.toggle("dis");
@@ -81,8 +74,6 @@ navBtn3.classList.add("active")
 navBtn4.classList.remove("active")
 contact.classList.remove("sec-dis")
 
-navBtn5.classList.remove("active")
-services.classList.remove("sec-dis")
 navBtn2.classList.remove("active")
 skills.classList.remove("sec-dis")
 nav.classList.toggle("dis");
@@ -94,27 +85,10 @@ portfolio.classList.remove("sec-dis")
 about.classList.remove("sec-dis")
 navBtn1.classList.remove("active")
 navBtn3.classList.remove("active")
-navBtn4.classList.add("active")
-contact.classList.remove("sec-dis")
-
-navBtn5.classList.remove("active")
-services.classList.add("sec-dis")
-navBtn2.classList.remove("active")
-skills.classList.remove("sec-dis")
-nav.classList.toggle("dis");
-document.body.classList.toggle("overflow");
-
-})
-navBtn5.addEventListener('click',() =>{
-portfolio.classList.remove("sec-dis")
-about.classList.remove("sec-dis")
-navBtn1.classList.remove("active")
-navBtn3.classList.remove("active")
 navBtn4.classList.remove("active")
 contact.classList.add("sec-dis")
 
-navBtn5.classList.add("active")
-services.classList.remove("sec-dis")
+navBtn4.classList.add("active")
 navBtn2.classList.remove("active")
 skills.classList.remove("sec-dis")
 nav.classList.toggle("dis");
